4f036636735fb46702db5b7359d1c551f414ffd9
John <supersweet098@gmail.com>
2023-03-12 17:56:06 -0600

    cool

---
+++ main.py
@@ -0,0 +1,33 @@
+import customtkinter
+import tkinter
+from tkinter.font import Font
+
+
+
+class App:
+    def __init__(self):
+        self.root = customtkinter.CTk()
+        self.root.title("Python Clock")
+        self.root.geometry("500x130")
+
+        # Main frame to hold label
+        self.main_frame = customtkinter.CTkFrame(self.root, fg_color="#282828")
+        self.main_frame.pack(pady=(20, 0))
+
+        # defining font 
+        self.font = Font(family="System")
+        # Main label to display values
+        self.time_var = tkinter.StringVar(value="02:09:59:AM")
+        self.number_label = customtkinter.CTkLabel(self.main_frame, textvariable=self.time_var, font=("Consolas", 75), text_color=("cyan"))
+        self.number_label.pack()
+
+
+        self.root.mainloop()
+
+    def time_function(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    app = App()
