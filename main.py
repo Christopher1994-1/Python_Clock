556cbb50dbaf47c7cf3d2e86d866d8f74ddab920
John <supersweet098@gmail.com>
2023-03-12 18:26:30 -0600

    got rid of useless code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import customtkinter
 import tkinter
-from tkinter.font import Font
 
 
 
@@ -14,8 +13,6 @@ class App:
         self.main_frame = customtkinter.CTkFrame(self.root, fg_color="#282828")
         self.main_frame.pack(pady=(20, 0))
 
-        # defining font 
-        self.font = Font(family="System")
         # Main label to display values
         self.time_var = tkinter.StringVar(value="02:09:59:AM")
         self.number_label = customtkinter.CTkLabel(self.main_frame, textvariable=self.time_var, font=("Consolas", 75), text_color=("cyan"))
